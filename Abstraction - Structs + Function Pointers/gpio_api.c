bec52f7d482b20507c494813a28c990a9f17b16e
Felipe Navarro <felnavarro2@gmail.com>
2018-02-13 01:53:38 -0200

    Update gpio_api.c

--- Abstraction - Structs + Function Pointers/gpio_api.c
+++ Abstraction - Structs + Function Pointers/gpio_api.c
@@ -3,14 +3,14 @@
 //PORT SET Function
 int set_fcn(int *GPIO_NUM)
 {
-	printf("GPIO %i SET \n", GPIO_NUM);
+	printf("GPIO %i SET \n", *GPIO_NUM);
 	return 0;
 }
 
 //PORT CLEAR Function
 int clear_fcn(int *GPIO_NUM)
 {   
- 	printf("GPIO %i CLEAR \n", GPIO_NUM);
+ 	printf("GPIO %i CLEAR \n", *GPIO_NUM);
 	return 0;
 }
 
